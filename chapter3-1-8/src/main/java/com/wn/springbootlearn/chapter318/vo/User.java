a950bfc3b1b71b378d81043fe1e52129194ffecf
wn <1450665043@qq.com>
2019-06-24 23:41:03 +0800

    xml消息转换器内定义对象与Xml的关系

--- chapter3-1-8/src/main/java/com/wn/springbootlearn/chapter318/vo/User.java
+++ chapter3-1-8/src/main/java/com/wn/springbootlearn/chapter318/vo/User.java
@@ -1,4 +1,23 @@
 package com.wn.springbootlearn.chapter318.vo;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * @Data、@NoArgsConstructor、@AllArgsConstructor是lombok简化代码的注解，主要用于生成get、set以及构造函数。
+ * @JacksonXmlRootElement、@JacksonXmlProperty注解是用来维护对象属性在xml中的对应关系。
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JacksonXmlRootElement(localName = "User")
 public class User {
+    @JacksonXmlProperty(localName = "name")
+    private String name;
+    @JacksonXmlProperty(localName = "age")
+    private Integer age;
 }
