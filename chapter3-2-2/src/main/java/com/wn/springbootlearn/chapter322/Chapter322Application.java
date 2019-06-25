54a3d4f9a250d4bd4738ad022f7e964bfb038a4b
wn <1450665043@qq.com>
2019-06-25 23:47:28 +0800

    Spring Boot中使用Spring-data-jpa让数据访问更简单、更优雅

---
+++ chapter3-2-2/src/main/java/com/wn/springbootlearn/chapter322/Chapter322Application.java
@@ -0,0 +1,13 @@
+package com.wn.springbootlearn.chapter322;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter322Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter322Application.class, args);
+    }
+
+}
